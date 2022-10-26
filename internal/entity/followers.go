240834bb5505c2f4de4738116ecf1fe01ba17165
lesha <lastchangenik@gmail.com>
2022-10-26 03:16:27 +0300

    add handler and struct row

--- internal/entity/followers.go
+++ internal/entity/followers.go
@@ -1,9 +1,11 @@
 package entity
 
+import "time"
+
 type Followers struct {
-	id         int    `json:"id"`
-	email      string `json:"email"`
-	name       string `json:"name"`
-	secondName string `json:"second_name"`
-	//birthDay data   тип данных дата
+	id         int       `json:"id"`
+	email      string    `json:"email"`
+	name       string    `json:"name"`
+	secondName string    `json:"second_name"`
+	birthDay   time.Time `json:"birth_day"`
 }
--- internal/handlers/api.go
+++ internal/handlers/api.go
@@ -1,7 +1,17 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
 
 func (h *Handler) SendEmail(c *gin.Context) {
-
+	arr, _ := c.GetQueryArray("arr[]")
+	htmlTemplates := c.Query("form")
+	id1, _ := strconv.Atoi(htmlTemplates)
+	c.JSON(http.StatusOK, gin.H{
+		"id1":  arr[0],
+		"form": id1,
+	})
 }
